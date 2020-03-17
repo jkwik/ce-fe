77c5b1053034143a05930bce8a92094a3fe65fb2
Naazish Sheikh <Naazish.Sheikh@gmail.com>
2020-03-17 13:18:21 -0400

    Created Coach Easy Nuxt project

---
+++ CoachEasy/pages/ResetPassword.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <PasswordResetForm></PasswordResetForm>
+  </div>
+</template>
+
+<script>
+import PasswordResetForm from '~/components/PasswordResetForm.vue'
+
+export default {
+  components: {
+    PasswordResetForm
+  }
+}
+</script>
+
+<style>
+
+</style>
